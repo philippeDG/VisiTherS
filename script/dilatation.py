c7ec4ab41b0bd44afba99c0f467cb9f79b95e013
philippeDG <philippe.d.guindon@gmail.com>
2022-07-22 14:33:10 -0400

    Adding dilatation study

---
+++ script/dilatation.py
@@ -0,0 +1,29 @@
+import PIL
+from PIL import Image
+import numpy as np
+import scipy.ndimage
+import sys
+
+def dilate_mask(image_array, iterations):
+
+    img = np.asarray(image_array)
+
+    if img.shape[-1] == 4:
+        image_last_dim = img[:,:,-1] // 255
+
+        dil_mask = scipy.ndimage.binary_dilation(image_last_dim, iterations=iterations)
+        img[:,:,-1] = np.uint8(dil_mask)*255
+
+    return img
+
+def erode_mask(image_array, iterations):
+
+    img = np.asarray(image_array)
+
+    if img.shape[-1] == 4:
+        image_last_dim = img[:,:,-1] // 255
+
+        dil_mask = scipy.ndimage.binary_erosion(image_last_dim, iterations=iterations)
+        img[:,:,-1] = np.uint8(dil_mask)*255
+
+    return img
